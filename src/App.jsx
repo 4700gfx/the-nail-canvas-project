5e88e8d9dcdc9b225982d182178988288e631fd8
47gfx <117771075+4700gfx@users.noreply.github.com>
2025-09-16 23:18:28 -0400

    Intial Files for Project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import Hero from './components/Hero'
+import Services from './components/Services'
+import Benefits from './components/Benefits'
+import Testimony from './components/Testimony'
+import Pricing from './components/Pricing'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Hero></Hero>
+      <Services></Services>
+      <Benefits></Benefits>
+      <Testimony></Testimony>
+      <Pricing></Pricing>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Benefits.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Benefits = () => {
+  return (
+    <div className='text-black'>Benefits</div>
+  )
+}
+
+export default Benefits
